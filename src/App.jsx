69e18aa31c693dc418cd3224cfa0676ca3abf862
Luke B <142310217+OGOZ111@users.noreply.github.com>
2023-12-29 17:30:41 +0200

    added input task bar, set states, passed states between parent and child, tested in console log, passing works fine

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import { ToDoWrapper } from "./components/ToDoWrapper";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <ToDoWrapper />
+    </>
+  );
 }
 
 export default App;
