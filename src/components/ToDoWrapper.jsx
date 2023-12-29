ff42514fc8cfc4e769e7312e10bbba37a8ef74ff
Luke B <142310217+OGOZ111@users.noreply.github.com>
2023-12-29 18:13:26 +0200

    added delete handler function

--- src/components/ToDoWrapper.jsx
+++ src/components/ToDoWrapper.jsx
@@ -6,17 +6,39 @@ uuidv4();
 
 export const ToDoWrapper = () => {
   const [todos, setTodos] = useState([]);
+
   const addTodo = (todo) => {
     setTodos([
       ...todos,
       { id: uuidv4(), task: todo, completed: false, isEditing: false },
-      console.log(todos),
     ]);
   };
+
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
   return (
     <div className="TodoWrapper">
+      <h1>Things to do</h1>
       <ToDoForm addTodo={addTodo} />
-      <Todo />
+
+      {todos.map((todo, index) => (
+        <Todo
+          task={todo}
+          key={index}
+          toggleComplete={toggleComplete}
+          deleteTodo={deleteTodo}
+        />
+      ))}
     </div>
   );
 };
